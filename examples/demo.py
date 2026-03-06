274cb6e363c4ed25f9bf97dcdc86fd76290edc0a
sueszli <jabaryyahya@gmail.com>
2026-03-06 07:54:12 +0100

    up

--- examples/demo.py
+++ examples/demo.py
@@ -10,18 +10,21 @@ from xdsl_exo.patches_llvmlite import jit_compile, to_llvmlite
 
 
 @proc
-def add(n: size, a: f32[n] @ DRAM, b: f32[n] @ DRAM, out: f32[n] @ DRAM):
-    for i in seq(0, n):
-        out[i] = a[i] + b[i]
+def matmul(C: f32[4, 4] @ DRAM, A: f32[4, 4] @ DRAM, B: f32[4, 4] @ DRAM):
+    for i in seq(0, 4):
+        for j in seq(0, 4):
+            C[i, j] = 0.0
+            for k in seq(0, 4):
+                C[i, j] += A[i, k] * B[k, j]
 
 
-engine = jit_compile(to_llvmlite(compile_procs([add])))
-fn = ctypes.CFUNCTYPE(None, ctypes.c_int64, ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p)(engine.get_function_address("add"))
+engine = jit_compile(to_llvmlite(compile_procs([matmul])))
+fn = ctypes.CFUNCTYPE(None, ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p)(engine.get_function_address("matmul"))
 
-n = 4
-a = np.array([1, 2, 3, 4], dtype=np.float32)
-b = np.array([10, 20, 30, 40], dtype=np.float32)
-out = np.zeros(n, dtype=np.float32)
+A = np.arange(16, dtype=np.float32).reshape(4, 4)
+B = np.eye(4, dtype=np.float32)
+C = np.zeros((4, 4), dtype=np.float32)
 
-fn(n, a.ctypes.data, b.ctypes.data, out.ctypes.data)
-print(out)
+fn(C.ctypes.data, A.ctypes.data, B.ctypes.data)
+assert np.allclose(C, A @ B)
+print(C)
