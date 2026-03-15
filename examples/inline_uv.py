20e8388a6074dd9fe319428cf02580b21eb62cf2
sueszli <jabaryyahya@gmail.com>
2026-03-15 21:13:28 +0100

    up

--- examples/inline_uv.py
+++ examples/inline_uv.py
@@ -2,6 +2,7 @@
 # requires-python = "==3.14.*"
 # dependencies = [
 #   "exojit @ git+https://github.com/sueszli/exojit.git",
+#   "numpy",
 # ]
 # ///
 
@@ -12,13 +13,13 @@ from exojit.main import compile_jit
 
 
 @proc
-def scale(out: f32[4] @ DRAM, alpha: f32[1] @ DRAM):
+def scale(out: f32[4] @ DRAM, factor: f32[1] @ DRAM):
     for i in seq(0, 4):
-        out[i] = alpha[0] * out[i]
+        out[i] = out[i] * factor[0]
 
 
 out = np.ones(4, dtype=np.float32)
-func = compile_jit(scale)["scale"]
-alpha = np.array([3.0], dtype=np.float32)
-func(out, alpha)
-print(out)
+factor = np.array([42.0], dtype=np.float32)
+scale = compile_jit(scale)["scale"](out, factor)
+assert list(out) == [42.0, 42.0, 42.0, 42.0]
+print("ok")
