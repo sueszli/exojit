2ed843c0e109e9c749c990e80f2acbf6b03b928e
Yahya Jabary <61852663+sueszli@users.noreply.github.com>
2026-09-05 21:06:24 +0200

    Update exojit.py

--- exojit.py
+++ exojit.py
@@ -46,9 +46,11 @@ from xdsl.transforms.reconcile_unrealized_casts import ReconcileUnrealizedCastsP
 from xdsl.utils.hints import isa
 from xdsl.utils.scoped_dict import ScopedDict
 
+
 # ===----------------------------------------------------------------------=== #
 # exo patches
 # ===----------------------------------------------------------------------=== #
+
 
 _pyparser._prim_types["size"] = UAST.Size()
 _pyparser._prim_types["index"] = UAST.Index()
@@ -87,6 +89,7 @@ def patched_lift_expr(e):
 
 
 _boundscheck.lift_expr = patched_lift_expr
+
 
 # ===----------------------------------------------------------------------=== #
 # xdsl patches
